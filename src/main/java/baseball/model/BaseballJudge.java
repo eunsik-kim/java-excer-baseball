836e4118a63f3bfcf77c7a067eaf97282287e92f
eunsik-kim <gracely9901@gmail.com>
2024-10-31 01:08:38 +0900

    feat: stike ball 판정 심판 개체 생성

---
+++ src/main/java/baseball/model/BaseballJudge.java
@@ -0,0 +1,39 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballJudge {
+    private final List<Integer> answer;
+    public BaseballJudge () {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        this.answer = computer;
+    }
+    public judgeNumberResult judgeNumber(List<Integer> Numbers) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < Numbers.size(); i++) {
+            int position = checkBallPosition(Numbers.get(i));
+            if (i == position) {
+                strike++;
+            } else if (position != -1) {
+                ball++;
+            }
+        }
+        return new judgeNumberResult(ball, strike);
+    }
+
+    private int checkBallPosition(Integer number) {
+        return this.answer.indexOf(number);
+    }
+
+    public record judgeNumberResult(Integer ball, Integer strike) { }
+}
---
+++ src/test/java/baseball/model/BaseballJudgeTest.java
@@ -0,0 +1,46 @@
+import baseball.model.BaseballJudge;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+
+class BaseballJudgeTest {
+    private BaseballJudge baseballJudge;
+
+    @Test
+    void 스트라이크_테스트() {
+        // Randoms.pickNumberInRange() 결과를 1,2,3으로 설정
+        assertRandomNumberInRangeTest(() -> {
+            baseballJudge = new BaseballJudge();
+            BaseballJudge.judgeNumberResult result =
+                    baseballJudge.judgeNumber(Arrays.asList(1, 2, 3));
+
+            assertThat(result.strike()).isEqualTo(3);
+            assertThat(result.ball()).isEqualTo(0);
+        }, 1, 2, 3);
+    }
+
+    @Test
+    void 볼_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            baseballJudge = new BaseballJudge();
+            BaseballJudge.judgeNumberResult result =
+                    baseballJudge.judgeNumber(Arrays.asList(3, 1, 2));
+
+            assertThat(result.strike()).isEqualTo(0);
+            assertThat(result.ball()).isEqualTo(3);
+        }, 1, 2, 3);
+    }
+
+    @Test
+    void 볼_스트라이크_혼합_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            baseballJudge = new BaseballJudge();
+            BaseballJudge.judgeNumberResult result =
+                    baseballJudge.judgeNumber(Arrays.asList(1, 3, 2));
+
+            assertThat(result.strike()).isEqualTo(1);
+            assertThat(result.ball()).isEqualTo(2);
+        }, 1, 2, 3);
+    }
+}
